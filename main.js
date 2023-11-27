20c366587ddba5cc7be17413cd88d22508ea577b
Lelik Jan <21alex7777@gmail.com>
2023-11-27 23:47:19 +0300

    Updates

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{var __webpack_modules__={579:()=>{eval('console.log("Hello World!");\n\n//# sourceURL=webpack://my-webpack-project/./src/index.js?')},633:()=>{"use strict";eval("// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://my-webpack-project/./src/sass/style.scss?")}};__webpack_modules__[579]();var __webpack_exports__={};__webpack_modules__[633]()})();
